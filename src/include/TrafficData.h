4a8e2e0ca0864fea7fb37329c49e4c78b6e79f3c
Samuel <xstola03@stud.fit.vutbr.cz>
2023-12-10 11:25:02 +0100

    Added Traffic data classes

---
+++ src/include/TrafficData.h
@@ -0,0 +1,62 @@
+/**
+ * @authors Samuel Stolarik, Jan Pavlicek
+ * @date 9-12-2023
+ * @file TrafficData.h
+ */
+
+#pragma once
+
+#include <vector>
+#include <string>
+
+struct TrafficDataSample {
+    float avg_speed;
+    float density;
+    float flux;
+    std::vector<std::string> road_snapshot;
+};
+
+class TrafficData {
+public:
+    TrafficData();
+
+    virtual
+    ~TrafficData() = default;
+
+    virtual
+    std::string to_csv() const = 0;
+
+    void add_sample(const TrafficDataSample& sample);
+
+protected:
+    /**
+     * Average speed of all vehicles on the road
+     */
+    std::vector<float> m_avg_speed;
+    /**
+     * Vehicles per step
+     */
+    std::vector<float> m_flux;
+
+    /**
+     * Occupied cells / all cells
+     */
+    std::vector<float> m_traffic_density;
+
+    /**
+     * Road snapshot
+     */
+    std::vector<std::vector<std::string>> m_road_snapshot;
+};
+
+class OneLaneTrafficData : public TrafficData {
+    OneLaneTrafficData() = default;
+
+    std::string to_csv() const override;
+};
+
+class TwoLaneTrafficData : public TrafficData {
+    TwoLaneTrafficData() = default;
+
+    std::string to_csv() const override;
+};
